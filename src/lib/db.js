0cd884e6e4ce51ee39cf8bfad91021ee151b3a0b
Darshitsepl <sepl.darshittarapara@gmail.com>
2025-11-29 00:59:30 +0530

    revert db config changes

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,9 +2,8 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const url = process.env.NODE_ENV == 'production' ?process.env.MONGO_PROD_URI :process.env.MONGO_URI
-    console.log(process.env.NODE_ENV,url,'url')
-    await mongoose.connect(url);
+    
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected");
   } catch (error) {
     console.error("MongoDB connection error:", error);
